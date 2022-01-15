02c3970c38da1f9977a170f06d7af64760a1c927
Trần Thành Long <ttlong21022000@gmail.com>
2022-01-15 14:20:03 +0700

    Outputting lists

--- src/Home.js
+++ src/Home.js
@@ -2,24 +2,35 @@ import { useState } from "react";
 
 const Home = () => {
   // let name = "mario";
-  const [name, setName] = useState("Mario");
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    // name = "Yos";
-    // console.log(name);
-    // change name but no re-render component
-    setName("lugi");
-    setAge(30);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        Name: {name} - Age: {age}
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
