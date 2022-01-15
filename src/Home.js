e2a1556b81a1e7b6b11b89ace87bf05bfb860079
Trần Thành Long <ttlong21022000@gmail.com>
2022-01-15 10:43:56 +0700

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello ninja", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("Hello " + name, " ", e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("Yos", e)}>Click Yos</button>
     </div>
   );
 };
