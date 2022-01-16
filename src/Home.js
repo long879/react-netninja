75325ffdb1131f41f8c2d92522eb4efd81990550
Trần Thành Long <ttlong21022000@gmail.com>
2022-01-16 10:10:17 +0700

    Handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,32 @@ const Home = () => {
   // terminal: npx json-server --watch data/db.json --port 8000
   const [blogs, setBlogs] = useState();
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
-        .then((res) => res.json())
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("could not fetch the data for that resource");
+          }
+          return res.json();
+        })
         .then((data) => {
           setBlogs(data);
           setIsPending(false);
+          setError();
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
