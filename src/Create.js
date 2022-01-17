bbcb13756d90a3c577d257a816ab90b2e7c939c8
Trần Thành Long <ttlong21022000@gmail.com>
2022-01-17 08:47:49 +0700

    Controlled inputs

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form>
+        <label htmlFor="">Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="">Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label htmlFor="">Blog author:</label>
+        <select value={author.name} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yos">yos</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
