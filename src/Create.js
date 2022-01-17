e9135a7f2c4995ff5dd35271c3a99fd1645ffb75
Trần Thành Long <ttlong21022000@gmail.com>
2022-01-17 11:15:51 +0700

    Making a post request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,20 @@ function Create() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, body, author };
-    console.log(blog);
+    setIsPending(true);
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+      setIsPending(false);
+    });
   };
 
   return (
@@ -33,7 +42,8 @@ function Create() {
           <option value="mario">mario</option>
           <option value="yos">yos</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
