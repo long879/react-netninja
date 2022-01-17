85fc0300e1d9141b9540c78b9cc3c2bb5b0d0ae7
Trần Thành Long <ttlong21022000@gmail.com>
2022-01-17 18:43:02 +0700

    Programmatic redirects

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Create() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,8 +17,8 @@ function Create() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog),
     }).then(() => {
-      console.log("new blog added");
       setIsPending(false);
+      navigate("/");
     });
   };
 
